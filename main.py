20f21650f7e0ee2895f4df713afece05ca8363a0
Jiggle090 <109288068+Jiggle090@users.noreply.github.com>
2025-12-05 21:54:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
-BOT_TOKEN = "8010597644:AAGiz57xHENfGp85jB8LlIumf-2ydfFxgdAE"
+BOT_TOKEN = "8010597644:AAFu1PX6c8TD8ci0VJ1fQiXdpJj1g6oI4RM"
 FREE_CREDITS = 3
 API_URL = "https://teraboxapi.com/api?url="
 
